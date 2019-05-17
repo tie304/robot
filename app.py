dc65a17ecd0f6714ca295e78b0d2423df96b93ed
Tyler Hanson <tyler@digilabs.io>
2019-05-17 16:39:43 -0500

    inital commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import time
+from pysabertooth import Sabertooth
+from robot import Robot
+
+PORT = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A105QI4I-if00-port0"
+
+
+saber = Sabertooth(PORT, baudrate=9600, address=128, timeout=0.1)
+
+
+robot = Robot(saber)
+
+
+print("going forward")
+robot.forward()
+time.sleep(3)
+print("Stopping robot")
+robot.stop()
+time.sleep(3)
+print("going backward")
+robot.backward()
+time.sleep(3)
+print("stopping")
+robot.stop()
---
+++ models/robot.py
@@ -0,0 +1,22 @@
+
+
+class Robot:
+    def __init__(diver):
+        self.driver = driver
+        self.forward_speed = 50
+        self.backward_speed = -50
+
+    def left(self):
+        pass
+
+    def right(self):
+        pass
+
+    def stop(self):
+        return self.driver.stop()        
+
+    def forward(self):
+        return self.diver.driveBoth(self.forward_speed,self.forward_speed)
+
+    def backward(self):
+        return self.diver.driveBoth(self.backward_speed,self.backward_speed)
