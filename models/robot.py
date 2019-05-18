f8dc2b24f1174fdfce721783219a78d85e3b0371
Tyler <tylerhanson921@gmail.com>
2019-05-17 23:34:49 -0500

    slowed turn rate

--- models/robot.py
+++ models/robot.py
@@ -5,14 +5,15 @@ class Robot:
         self.driver = driver
         self.forward_speed = -50
         self.backward_speed = 50
+        self.turn_slow = 2
 
     def left(self):
-        self.driver.drive(1,self.backward_speed)
-        self.driver.drive(2,self.forward_speed)
+        self.driver.drive(1,self.backward_speed / self.turn_slow)
+        self.driver.drive(2,self.forward_speed / self.turn_slow)
 
     def right(self):
-        self.driver.drive(1,self.forward_speed)
-        self.driver.drive(2,self.backward_speed)
+        self.driver.drive(1,self.forward_speed / self.turn_slow)
+        self.driver.drive(2,self.backward_speed / self.turn_slow)
 
     def stop(self):
         return self.driver.stop()
