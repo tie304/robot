136465dae44d4e4ffff5ca212f455dcfb28ffbab
Tyler Hanson <tyler@digilabs.io>
2019-05-20 12:20:34 -0500

    adding turning

--- models/robot.py
+++ models/robot.py
@@ -9,12 +9,12 @@ class Robot:
         self.offset = .10
 
     def left(self):
-        self.driver.drive(1,self.backward_speed / self.turn_slow)
-        self.driver.drive(2,self.forward_speed / self.turn_slow)
+        self.driver.drive(1,self.forward_speed - 15)
+        self.driver.drive(2,self.forward_speed)
 
     def right(self):
-        self.driver.drive(1,self.forward_speed / self.turn_slow)
-        self.driver.drive(2,self.backward_speed / self.turn_slow)
+        self.driver.drive(1,self.forward_speed)
+        self.driver.drive(2,self.backward_speed - 15)
 
     def stop(self):
         return self.driver.stop()
